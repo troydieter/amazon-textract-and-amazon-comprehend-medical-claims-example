259e3290f4b67742d58c6fe23dcd633943e7b06d
Troy Dieter <troydieter@gmail.com>
2021-06-07 21:36:11 -0400

    fixed misspell

--- tf/lambda.tf
+++ tf/lambda.tf
@@ -78,7 +78,7 @@ resource "aws_lambda_function" "extract" {
   tags = local.common-tags
   depends_on = [
     aws_s3_bucket_object.parse-desc,
-    aws_s3_bucket_object.extract,
+    aws_s3_bucket_object.extract-queue,
     aws_s3_bucket_object.validate
   ]
 }
