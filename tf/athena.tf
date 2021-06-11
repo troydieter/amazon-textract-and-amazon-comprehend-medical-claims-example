05ed6021aba9ae2479e1c6cc5f84c38551c29c47
Troy Dieter <troydieter@gmail.com>
2021-06-10 20:52:50 -0400

    working on athena

--- tf/athena.tf
+++ tf/athena.tf
@@ -14,6 +14,43 @@ resource "aws_athena_workgroup" "analyze" {
 resource "aws_glue_catalog_table" "aws_glue_catalog_table" {
   name          = "glue${random_id.rando.hex}"
   database_name = aws_athena_database.analyze-db.id
+  table_type    = "EXTERNAL_TABLE"
+  parameters = {
+    EXTERNAL = "TRUE"
+  }
+
+  storage_descriptor {
+    location      = "s3://${aws_s3_bucket.resultbucket.id}/result/"
+
+    ser_de_info {
+      name                  = "gluestr${random_id.rando.hex}"
+      serialization_library = "org.apache.hadoop.hive.serde2.OpenCSVSerde"
+
+      parameters {
+        separatorChar = ","
+        quoteChar = "\""
+        escapeChar = "\\"
+      }
+    }
+
+    columns {
+      name = "idnumber"
+      type = "string"
+    }
+
+    columns {
+      name = "category"
+      type = "string"
+    }
+
+    columns {
+      name = "text"
+      type = "string"
+    }
+
+  }
+
+
 }
 
 resource "aws_athena_database" "analyze-db" {
@@ -25,5 +62,5 @@ resource "aws_athena_named_query" "analyze-query" {
   name      = "query-${random_id.rando.hex}"
   workgroup = aws_athena_workgroup.analyze.id
   database  = aws_athena_database.analyze-db.name
-  query     = "SELECT * FROM ${aws_athena_database.analyze-db.name} limit 10;"
+  query     = "SELECT * FROM ${aws_athena_database.analyze-db.name}.${aws_glue_catalog_table.aws_glue_catalog_table.name} where category='TEST_TREATMENT_PROCEDURE' and text like '%glucose%'"
 }
